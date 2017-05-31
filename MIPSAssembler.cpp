bd5eaef85584875f349700dc4bd933d5a37b8e0e
Victor Hermann Chiletto <victor.vasconceloschiletto@gmail.com>
2017-05-31 00:44:00 -0300

    Hopefully finish the AST?

--- MIPSAssembler.cpp
+++ MIPSAssembler.cpp
@@ -5,6 +5,7 @@
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/phoenix_fusion.hpp>
 #include <boost/spirit/include/phoenix_stl.hpp>
+#include <boost/spirit/include/phoenix_object.hpp>
 
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/io.hpp>
@@ -18,7 +19,14 @@ namespace phoenix = boost::phoenix;
 // good ol' MIPS instruction
 struct Instruction {
     std::string name;
+    std::vector<std::string> arguments;
 };
+
+BOOST_FUSION_ADAPT_STRUCT(
+        Instruction,
+        (std::string, name),
+        (std::vector<std::string>, arguments)
+)
 
 // assembler directives. things like .asciiz
 struct Directive {
@@ -26,23 +34,33 @@ struct Directive {
     std::string argument;
 };
 
+BOOST_FUSION_ADAPT_STRUCT(
+        Directive,
+        (std::string, name),
+        (std::string, argument)
+)
+
 // Address labels
 struct Label {
     std::string name;
 };
 
+BOOST_FUSION_ADAPT_STRUCT(
+        Label,
+        (std::string, name)
+)
+
+using Instruction_or_Directive = boost::variant<Instruction, Directive>;
 // A line. Contains an optional label and either an instruction or a directive.
 struct Line {
-    /*
     boost::optional<Label> label;
-    boost::variant<Instruction, Directive> IoD;
-    */
-    std::string help_me;
+    Instruction_or_Directive IoD;
 };
 
 BOOST_FUSION_ADAPT_STRUCT(
         Line,
-        (std::string, help_me)
+        (boost::optional<Label>, label),
+        (Instruction_or_Directive, IoD)
 )
 
 // The root of the AST. Basically an array of lines
@@ -64,7 +82,7 @@ struct skipper : qi::grammar<Iterator>
         ascii::space_type space;
 
         start =
-            (space - qi::eol) | '#' >> *(char_ - qi::eol);
+            (space - qi::eol) | ('#' >> *(char_ - qi::eol));
     }
 
     qi::rule<Iterator> start;
@@ -74,19 +92,91 @@ template <typename Iterator, typename Skipper = skipper<Iterator>>
 struct AsmGrammar : qi::grammar<Iterator, MipsAsm(), Skipper> {
     AsmGrammar() : AsmGrammar::base_type(root)
     {
-        using qi::lit;
         using ascii::char_;
         using qi::_1;
-        using boost::spirit::no_skip;
+        using qi::lexeme;
         using phoenix::push_back;
         using phoenix::at_c;
+        using qi::as_string;
+        using phoenix::val;
         using namespace qi::labels;
 
-        line = *(char_ - qi::eol) >> qi::eol;
+        directive = 
+            '.' 
+            >> lexeme 
+            [ 
+                as_string
+                [
+                    (
+                        +(
+                            ~char_(' ')
+                        )
+                    )
+                ] [at_c<0>(_val) += _1]
+                >> ' '
+            ]
+            >> +(char_ - qi::eol) [at_c<1>(_val) += _1];
+
+        instruction = 
+            lexeme 
+            [
+                as_string
+                [
+                    (
+                        +(
+                            ~char_(' ')
+                        )
+                    )
+                ] [at_c<0>(_val) = _1]
+                >> ' '
+            ]
+            >> 
+            *(
+                as_string
+                [
+                    (
+                        +(
+                            ~char_(',')
+                        )
+                    )
+                ] [push_back(at_c<1>(_val), _1)]
+                >> -char_(',')
+            );
+
+        label =
+            *(
+                ~char_(':')
+            )
+            >> ':';
+
+        line = 
+            -label [at_c<0>(_val) = _1]
+            >> (directive | instruction) [at_c<1>(_val) = _1]
+            >> -qi::eol;
 
         root =
-            *(line) [push_back(at_c<0>(_val), _1)];
+            +(line) [push_back(at_c<0>(_val), _1)];
+
+        /*
+         * DEBUG GARBO
+         *
+        root.name("root");
+        line.name("line");
+        label.name("label");
+        directive.name("directive");
+        instruction.name("instruction");
+
+        debug(root);
+        debug(line);
+        debug(label);
+        debug(directive);
+        debug(instruction);
+        */
     }
+
+    qi::rule<Iterator, Directive(), Skipper> directive;
+    qi::rule<Iterator, Instruction(), Skipper> instruction;
+    qi::rule<Iterator, Label(), Skipper> label;
     qi::rule<Iterator, Line(), Skipper> line;
     qi::rule<Iterator, MipsAsm(), Skipper> root;
 };
@@ -101,11 +191,9 @@ int main()
     grammar g;
     Skipper sp;
 
-    std::string asd = "helicoptero #comentario\n"
-        "li $t0, 10 #comentario 2\n";
+    std::string asd = "teste: .helicoptero voando\nsuamae aquela,puta";
     auto res2 = qi::phrase_parse(asd.begin(), asd.end(), g, sp, asm_);
 
 
     return 0;
 }
-
